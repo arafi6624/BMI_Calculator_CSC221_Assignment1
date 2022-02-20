995f16357abbf4d3ae87bbe63100367ceab25404
Abdul Rafi <arafi6624@bths.edu>
2022-02-20 10:42:29 -0500

    Comments Completed

--- src/BMI.java
+++ src/BMI.java
@@ -5,32 +5,49 @@ import java.util.Calendar;
  * 1. calculateAge
  * 2. calculateBMI
  * 3. classifyBMI
- * All three methods take in the Person object as a parameter and uses its methods to calculate the age, BMI, and BMI classification of the object.
+ * All three methods take in the Person object as a parameter and uses its methods to calculate the age,
+ * BMI, and BMI classification of the object.
  *
  * @author Abdul Rafi
  * @version 1.0
  */
 public class BMI {
+
+    /**
+     * Calculates the age of a person by using the getBirthYear method of the person class and the current year.
+     * @param person A Person class object
+     * @return Returns the age of the person as an int
+     */
     public int calculateAge(final Person person) {
-        Calendar calendar = Calendar.getInstance();
-        int currentYear = calendar.get(Calendar.YEAR);
-        return currentYear - person.getBirthYear();
+        Calendar calendar = Calendar.getInstance(); //Initializes the calendar object
+        int currentYear = calendar.get(Calendar.YEAR); //Retrieves the current year
+        return currentYear - person.getBirthYear(); //Returns the calculated age
     }
 
+    /**
+     * Calculates the BMI by using the Person class methods getWeight and getHeight.
+     * @param person A Person class object
+     * @return Returns the BMI of a person as a double
+     */
     public double calculateBMI(final Person person) {
-        return person.getWeight()/(person.getHeight() * person.getHeight());
+        return person.getWeight()/(person.getHeight() * person.getHeight()); //Returns the calculated BMI
     }
 
+    /**
+     * Classifies the BMI of a person by using the calculateBMI method
+     * @param person A Person class object
+     * @return Returns a String classification of the calculated BMI based on a pre-determined index
+     */
     public String classifyBMI(final Person person) {
-        double BMI = calculateBMI(person);
+        double BMI = calculateBMI(person); //Passes the person object into the calculateBMI method and stores the output
         if(BMI < 18.5) {
-            return "Underweight";
+            return "Underweight"; //Returns 'underweight' if BMI is under 18.5
         } else if(BMI >= 18.5 && BMI < 25) {
-            return "Normal Weight";
+            return "Normal Weight"; //Returns 'Normal Weight' if BMI is between 18.5 and 25
         } else if(BMI >= 25 && BMI <= 30) {
-            return "Overweight";
+            return "Overweight"; //Returns 'Overweight' if BMI is between 25 and 30
         } else {
-            return "Obese";
+            return "Obese"; //Returns 'Obese' if failed to return any of the above classifications
         }
     }
 }
--- src/TestA1.java
+++ src/TestA1.java
@@ -1,73 +1,84 @@
 import java.util.Scanner;
 
+/**
+ * This class contains the main method which tests the Person and BMI classes.
+ *
+ * @author Abdul Rafi
+ * @version 1.0
+ */
 public class TestA1 {
+
+    /**
+     * The main method that executes when the class is compiled.
+     * @param args Any command-line arguments passed in when the class is run
+     */
     public static void main(String[] args) {
 
-        Person person1 = new Person();
+        Person person1 = new Person(); //Initializes a new Person class object
 
-        Scanner keyboard = new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in); //Initializes a new Scanner object for command-line inputs
 
         System.out.printf("First Instance Prompts\n");
 
-        System.out.printf("Enter person's name: ");
-        person1.setFullName(keyboard.nextLine());
+        System.out.printf("Enter person's name: "); //Prompts the user to enter a person's full name
+        person1.setFullName(keyboard.nextLine()); //Takes in an input from the user and passes it as a parameter for the setFullName method of the person1 object
 
-        System.out.printf("Enter person's year of birth: ");
-        person1.setBirthYear(keyboard.nextInt());
+        System.out.printf("Enter person's year of birth: "); //Prompts the user to enter a person's birth year
+        person1.setBirthYear(keyboard.nextInt()); //Takes in an input from the user and passes it as a parameter for the setBirthYear method of the person1 object
 
-        System.out.printf("Enter person's height in meters: ");
-        person1.setHeight(keyboard.nextDouble());
+        System.out.printf("Enter person's height in meters: "); //Prompts the user to enter a person's height
+        person1.setHeight(keyboard.nextDouble()); //Takes in an input from the user and passes it as a parameter for the setHeight method of the person1 object
 
-        System.out.printf("Enter person's weight in kilograms: ");
-        person1.setWeight(keyboard.nextDouble());
+        System.out.printf("Enter person's weight in kilograms: "); //Prompts the user to enter a person's weight
+        person1.setWeight(keyboard.nextDouble()); //Takes in an input from the user and passes it as a parameter for the setWeight method of the person1 object
         keyboard.nextLine();
 
         System.out.printf("\nSecond Instance Prompts\n");
 
-        System.out.printf("Enter person's name: ");
-        String person2FullName = keyboard.nextLine();
+        System.out.printf("Enter person's name: "); //Prompts the user to enter a person's name
+        String person2FullName = keyboard.nextLine(); //Takes in an input from the user and stores it in the String variable person2FullName
 
-        System.out.printf("Enter person's year of birth: ");
-        int person2BirthYear = keyboard.nextInt();
+        System.out.printf("Enter person's year of birth: "); //Prompts the user to enter a person's birth year
+        int person2BirthYear = keyboard.nextInt(); //Takes in an input from the user and stores it in the int variable person2BirthYear
 
-        System.out.printf("Enter person's height in meters: ");
-        double person2Height = keyboard.nextDouble();
+        System.out.printf("Enter person's height in meters: "); //Prompts the user to enter a person's height
+        double person2Height = keyboard.nextDouble(); //Takes in an input from the user and stores it in the double variable person2Height
 
-        System.out.printf("Enter person's weight in kilograms: ");
-        double person2Weight = keyboard.nextDouble();
+        System.out.printf("Enter person's weight in kilograms: "); //Prompts the user to enter a person's weight
+        double person2Weight = keyboard.nextDouble(); //Takes in an input from the user and stores it in the double variable person2Weight
 
-        keyboard.close();
+        keyboard.close(); //Closes the Scanner keyboard
 
-        Person person2 = new Person(person2FullName, person2BirthYear, person2Height, person2Weight);
+        Person person2 = new Person(person2FullName, person2BirthYear, person2Height, person2Weight); //Initializes the person2 object with specified parameters
 
-        BMI bmi = new BMI();
+        BMI bmi = new BMI(); //Initializes a BMI class object
 
         System.out.printf("\n1st Instance Output\n");
         System.out.printf("%16s", "Full Name: ");
-        System.out.printf("%s%n", person1.getFullName());
+        System.out.printf("%s%n", person1.getFullName()); //Prints the full name from the person1 object using the getFullName method
         System.out.printf("%16s", "Age: ");
-        System.out.printf("%s%n", bmi.calculateAge(person1));
+        System.out.printf("%s%n", bmi.calculateAge(person1)); //Calculates the age by passing in the person1 object into the calculateAge method of bmi and prints it
         System.out.printf("%16s", "Year: ");
-        System.out.printf("%s%n", person1.getBirthYear());
+        System.out.printf("%s%n", person1.getBirthYear()); //Prints the birth year from the person1 object using the getBirthYear method
         System.out.printf("%16s", "Height: ");
-        System.out.printf("%.2f%n", person1.getHeight());
+        System.out.printf("%.2f%n", person1.getHeight()); //Prints the height from the person1 object using the getHeight method
         System.out.printf("%16s", "Weight: ");
-        System.out.printf("%.2f%n", person1.getWeight());
+        System.out.printf("%.2f%n", person1.getWeight()); //Prints the weight from the person1 object using the getWeight method
         System.out.printf("%16s", "Classification: ");
-        System.out.printf("%s%n", bmi.classifyBMI(person1));
+        System.out.printf("%s%n", bmi.classifyBMI(person1)); //Classifies the BMI using the person1 object as the parameter for the classifyBMI method and prints it
 
         System.out.printf("\n2nd Instance Output\n");
         System.out.printf("%16s", "Full Name: ");
-        System.out.printf("%s%n", person2.getFullName());
+        System.out.printf("%s%n", person2.getFullName()); //Prints the full name from the person2 object using the getFullName method
         System.out.printf("%16s", "Age: ");
-        System.out.printf("%s%n", bmi.calculateAge(person2));
+        System.out.printf("%s%n", bmi.calculateAge(person2)); //Calculates the age by passing in the person2 object into the calculateAge method of bmi and prints it
         System.out.printf("%16s", "Year: ");
-        System.out.printf("%s%n", person2.getBirthYear());
+        System.out.printf("%s%n", person2.getBirthYear()); //Prints the birth year from the person2 object using the getBirthYear method
         System.out.printf("%16s", "Height: ");
-        System.out.printf("%.2f%n", person2.getHeight());
+        System.out.printf("%.2f%n", person2.getHeight()); //Prints the height from the person2 object using the getHeight method
         System.out.printf("%16s", "Weight: ");
-        System.out.printf("%.2f%n", person2.getWeight());
+        System.out.printf("%.2f%n", person2.getWeight()); //Prints the weight from the person2 object using the getWeight method
         System.out.printf("%16s", "Classification: ");
-        System.out.printf("%s%n", bmi.classifyBMI(person2));
+        System.out.printf("%s%n", bmi.classifyBMI(person2)); //Classifies the BMI using the person2 object as the parameter for the classifyBMI method and prints it
     }
 }
